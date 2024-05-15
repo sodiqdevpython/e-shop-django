44fa488d89dfce59ed18f35e431f1576b07c9f14
Shoxrux <sodiqdevpython@gmail.com>
2024-05-15 16:54:35 +0500

    added logout function

--- eShopApp/urls.py
+++ eShopApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import home, store, filtering_products, search, detail_products, checkout, delete_order, login
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', home, name='home'),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('mahsulot/<int:id>/', detail_products, name='product_detail'),
     path('checkout/', checkout, name='checkout'),
     path('delete_order/<int:id>/', delete_order, name='delete_order'),
-    path('login/', login, name='login')
+    path('login/', login, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
